f958e5237dc80fd658c78f3d16c7351848c09de9
Justin <jrohweller13@apu.edu>
2022-06-18 02:40:23 -0700

    deleted root.tsx

--- src/root/index.ts
+++
@@ -1,9 +0,0 @@
-import Root from "./Root";
-
-export { LoadingContext } from "./RootLoadingProvider";
-export { default as useCacheImages } from "./useCacheImages";
-export { default as useKeepSplash } from "./useKeepSplash";
-export { default as useLinkingUrl } from "./useLinkingUrl";
-export { default as useOTAUpdateStatus } from "./useOTAUpdateStatus";
-export { default as useWatchMemory } from "./useWatchMemory";
-export default Root;
