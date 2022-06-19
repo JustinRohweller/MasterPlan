45e7f996290ecb1dac2611f6ef72fd664fd990f5
Justin <jrohweller13@apu.edu>
2022-06-19 00:42:24 -0700

    p

--- src/navigator/MainNavigator/MainNavigator.tsx
+++ src/navigator/MainNavigator/MainNavigator.tsx
@@ -3,6 +3,39 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import * as navigators from "../../navigator";
 import * as screens from "../../screens";
 
+// TODO: DRY THIS MORE. ie. this and global stack are super simliar.
+// maybe pass in an object of the type of navigation structure?
+// ie.
+
+// goal is just to say how the navigators are configured.
+
+// name of screen, navigator it belongs to
+// const navigationStructure = {
+//   auth: {
+//     login: "Switch",
+//     signUp: "Switch",
+//     globalStack: {
+//       drawerNav: {
+
+//       }
+//       profileDetails: "Stack"
+//       tabs: {
+//         home: "Tab",
+//         profile: "Tab",
+//         settings: "Tab",
+//       },
+//     },
+//   },
+// };
+
+const navigationStructure2 = {
+  Switch: {
+    Stack: {
+      Drawer: true,
+      Tabs: true,
+    },
+  },
+};
 const Tab = createBottomTabNavigator();
 
 const navigatorScreens = Object.values(navigators).map((item, index: number) =>
