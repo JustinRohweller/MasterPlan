2473c248293dd4f0dfe975bf0279331c10846fb7
Justin <jrohweller13@apu.edu>
2022-06-21 20:25:30 -0700

    p

--- Navigator.tsx
+++ Navigator.tsx
@@ -1,13 +1,3 @@
-// Renders extras
-// ie. stuff all the app can access: public.
-// Perhaps rename to publicNavigator
-// stacknav to random user profile
-// stacknav to public winnings, ect.
-
-// https://reactnavigation.org/docs/stack-navigator
-
-// https://akveo.github.io/react-native-ui-kitten/docs/guides/configure-navigation#configure-navigation
-
 import { NavigationCreator } from "@jrohweller/mycomponents.ui.navigator";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
