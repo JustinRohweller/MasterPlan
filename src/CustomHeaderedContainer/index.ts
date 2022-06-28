23723c36aab13e091a3afaf2f09fefc263cf8919
Justin <jrohweller13@apu.edu>
2022-06-28 03:00:10 -0700

    p

--- src/CustomHeaderedContainer/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./CustomHeader";
-export { default as CustomHeader } from "./CustomHeader";
-export * from "./CustomHeaderedContainer";
-export { default as CustomHeaderedContainer } from "./CustomHeaderedContainer";
